2ac0614628981b36cfc6d349441f0119fa3cd0fa
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-09-27 00:41:14 +0300

    Updates

---
+++ precache-manifest.0845c103158209958942dbff108650cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fe2a17affa816534d32cce8dcc68b3",
+    "url": "/forBootcamp14/index.html"
+  },
+  {
+    "revision": "43086b452a1a3740e9cf",
+    "url": "/forBootcamp14/static/css/main.15c97f02.chunk.css"
+  },
+  {
+    "revision": "cc27838c5b313ff45751",
+    "url": "/forBootcamp14/static/js/2.7f5b6cce.chunk.js"
+  },
+  {
+    "revision": "43086b452a1a3740e9cf",
+    "url": "/forBootcamp14/static/js/main.719d2730.chunk.js"
+  },
+  {
+    "revision": "30a435a5072700d206bb",
+    "url": "/forBootcamp14/static/js/runtime-main.584a7a03.js"
+  }
+]);
